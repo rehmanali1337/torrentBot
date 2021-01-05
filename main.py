2c8c03b23e89b3b4c08845f7fc3f0230dbca52e6
Rehman Ali <rehmanali.9442289@gmail.com>
2021-01-05 12:06:38 +0500

    update

--- main.py
+++ main.py
@@ -55,6 +55,8 @@ async def startAdminConv(event):
 
 
 async def startUserConv(event):
+    en = await client.get_entity(event.peer_id)
+    print(en.stringify())
     conversation = UserConversation(
         bot, client, torrentsQueue, ytQueue, megaQueue)
     await conversation.start(event)
